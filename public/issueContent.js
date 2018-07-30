63be1de5ac1a2d75a2eb397c13d92168c35b50e6
MTaylorCSE <mrt001@ucsd.edu>
2018-07-29 20:26:28 -0700

    Dynamically loaded contents of issue list.

---
+++ public/issueContent.js
@@ -0,0 +1,96 @@
+var issue_contents_1 = {
+  issue_name:"Terrible Logo",
+  issue_type:"Visual",
+  issue_description:"This logo looks like someone attacked a box of crayons\nwith a curling iron. Whoever drew this needs a stern\ntalking to.",
+  issue_image_src:"logo.gif"
+}
+
+var issue_contents_2 = {
+  issue_name:"Terrible Logo Broken Image",
+  issue_type:"Visual",
+  issue_description:"Now the terrible logo that idiot drew won't even show up\non the issue page. I bet he wrote the code for this website\ntoo, judging by the looks of it.",
+  issue_image_src:"issue2_1.png"
+}
+
+var issue_contents_3 = {
+  issue_name:"WebP Image in Firefox",
+  issue_type:"Visual",
+  issue_description:"Look at this picture of a cat. It's beautiful. It\nisn't as beautiful in Firefox. There, it's just my\nalt text.",
+  issue_image_src:"nocat.jpeg"
+}
+
+var issues = [issue_contents_1,issue_contents_2,issue_contents_3];
+
+function loadIssueContents(issue_number) {
+
+  issue_contents = issues[issue_number - 1];
+  var issueDescription = document.getElementById('issueDescription');
+  var issueName_title = document.getElementById('issueName_title');
+  var issueName_heading = document.getElementById('issueName_heading');
+  var issueType = document.getElementById('issueType');
+  var defaultOptionIssueType = document.getElementById('defaultOptionIssueType');
+  var issueImage = document.getElementById('issueImage');
+
+  issueType.innerText = issue_contents.issue_type;
+  issueName_title.innerText = issue_contents.issue_name;
+  issueName_heading.innerText = issue_contents.issue_name;
+  issueDescription.innerText = issue_contents.issue_description;
+  defaultOptionIssueType.innerText = issue_contents.issue_type;
+  issueImage.src = issue_contents.issue_image_src;
+}
+
+
+var issueDescription = document.getElementById('issueDescription');
+var saveButton = document.getElementById('saveButton');
+var cancelButton = document.getElementById('cancelButton');
+var editButton = document.getElementById('editButton');
+var issueName_title = document.getElementById('issueName_title');
+var issueName_heading = document.getElementById('issueName_heading');
+var issueType = document.getElementById('issueType');
+var selectIssueType = document.getElementById('selectIssueType');
+var defaultOptionIssueType = document.getElementById('defaultOptionIssueType');
+var preEditIssueType;
+var preEditContent;
+var preEditIssueName;
+
+function editMode(){
+  editButton.hidden = true;
+  saveButton.hidden = false;
+  cancelButton.hidden = false;
+  issueType.hidden = true;
+  selectIssueType.hidden = false;
+  issueDescription.contentEditable = true;
+  issueName_heading.contentEditable = true;
+  preEditIssueType = issueType.innerText;
+  preEditContent = issueDescription.innerText;
+  preEditIssueName = issueName_heading.innerText;
+}
+
+function saveEdit(){
+  editButton.hidden = false;
+  saveButton.hidden = true;
+  cancelButton.hidden = true;
+  var newIssueType = selectIssueType.selectedOptions.item(0).innerText;
+  issueType.innerText = newIssueType;
+  defaultOptionIssueType.innerText = newIssueType;
+  issueType.hidden = false;
+  selectIssueType.hidden = true;
+  issueDescription.contentEditable = false;
+  issueName_heading.contentEditable = false;
+  preEditContent = issueDescription.innerText;
+  preEditIssueName = issueName_heading.innerText;
+  issueName_title.innerText = issueName_heading.innerText;
+}
+
+function cancelEdit(){
+  editButton.hidden = false;
+  saveButton.hidden = true;
+  cancelButton.hidden = true;
+  selectIssueType.hidden = true;
+  issueType.hidden = false;
+  defaultOptionIssueType.selected = true;
+  issueDescription.contentEditable = false;
+  issueDescription.innerText = preEditContent;
+  issueName_heading.contentEditable = false;
+  issueName_heading.innerText = preEditIssueName;
+}
---
+++ public/issueList.js
@@ -0,0 +1,135 @@
+var issueList = [
+  {
+    status:"open",
+    issueID:1,
+    issueName:"Terrible Logo",
+    issueType:"Visual",
+    issueDescription:"Who even drew this?",
+
+  },
+  {
+    status:"open",
+    issueID:2,
+    issueName:"Terrible Logo Broken Image",
+    issueType:"Visual",
+    issueDescription:"Great, the logo we didn't even want to see isn't appearing on the page.",
+
+  },
+  {
+    status:"open",
+    issueID:3,
+    issueName:"WebP Image in Firefox",
+    issueType:"Visual",
+    issueDescription:"This WebP image of a cat doesn't work in Firefox.",
+
+  }
+
+];
+window.addEventListener("load",loadIssues);
+
+function loadIssues(){
+  var issueListBody = document.getElementById("issueListBody");
+  var i;
+  for(i = 0; i < issueList.length; i++){
+    var issueID = issueList[i].issueID;
+    var newRow = document.createElement("tr");
+    newRow.setAttribute("class","listItem_" + issueList[i].status);
+    newRow.setAttribute("id","listItem"+issueID);
+
+    var idCell = document.createElement("td");
+    idCell.appendChild(document.createTextNode(issueID));
+    newRow.appendChild(idCell);
+
+    var issueLink = document.createElement("a");
+    issueLink.setAttribute("href","issue.html?issue="+issueID);
+    issueLink.appendChild(document.createTextNode(issueList[i].issueName));
+    
+    var issueLinkCell = document.createElement("td");
+    issueLinkCell.appendChild(issueLink);
+    newRow.appendChild(issueLinkCell);
+
+    var issueTypeCell = document.createElement("td");
+    issueTypeCell.appendChild(document.createTextNode(issueList[i].issueType));
+    newRow.appendChild(issueTypeCell);
+
+    var issueDescriptionCell = document.createElement("td");
+    issueDescriptionCell.appendChild(document.createTextNode(issueList[i].issueDescription));
+    newRow.appendChild(issueDescriptionCell);
+    
+    var closeOrOpenButton = document.createElement("button");
+    closeOrOpenButton.setAttribute("id","resolve"+issueID);
+    if(issueList[i].status == "open"){
+      closeOrOpenButton.setAttribute("onclick","closeIssue("+issueID+")");
+      closeOrOpenButton.appendChild(document.createTextNode("Close Issue"));
+    } else {
+      closeOrOpenButton.setAttribute("onclick","openIssue("+issueID+")");
+      closeOrOpenButton.appendChild(document.createTextNode("Open Issue"));
+    }
+    
+    closeOrOpenButtonCell = document.createElement("td");
+    closeOrOpenButtonCell.appendChild(closeOrOpenButton);
+    newRow.appendChild(closeOrOpenButtonCell);
+
+    var deleteButton = document.createElement("button");
+    deleteButton.setAttribute("onclick","openDeletionConfirmation("+issueID+")");
+    deleteButton.appendChild(document.createTextNode("Delete"));
+
+    var deleteButtonCell = document.createElement("td");
+    deleteButtonCell.appendChild(deleteButton);
+    newRow.appendChild(deleteButtonCell);
+
+    issueListBody.appendChild(newRow);
+  }
+}
+
+function closeIssue(item){
+  var itemToClose = document.getElementById("listItem" + item);
+  itemToClose.style = "font-style: italic";
+  itemToClose.className = "listItem_closed";
+  var resolveButton = document.getElementById("resolve"+item);
+  resolveButton.innerText = "Open Issue";
+  resolveButton.onclick = function(){openIssue(item)};
+}
+function openIssue(item){
+  var itemToOpen = document.getElementById("listItem" + item);
+  itemToOpen.style = "font-style: normal";
+  itemToOpen.className = "listItem_open";
+  var resolveButton = document.getElementById("resolve"+item);
+  resolveButton.innerText = "Close Issue";
+  resolveButton.onclick = function(){closeIssue(item)};
+}
+function showAllIssues(){
+  var showIssuesCheckbox = document.getElementById("showIssuesCheckbox");
+  var closedIssues = document.getElementsByClassName("listItem_closed");
+  var i = 0;
+  for(i; i < closedIssues.length; i++){
+      closedIssues.item(i).hidden = false;
+  }
+  showIssuesCheckbox.onclick = function(){showOnlyOpenIssues()};
+}
+function showOnlyOpenIssues(){
+  var showIssuesCheckbox = document.getElementById("showIssuesCheckbox");
+  var closedIssues = document.getElementsByClassName("listItem_closed");
+  var i = 0;
+  for(i; i < closedIssues.length; i++){
+      closedIssues.item(i).hidden = true;
+  }
+  showIssuesCheckbox.onclick = function(){showAllIssues()};
+}
+var numIssueToDelete;
+function openDeletionConfirmation(item){
+  var deletionModal = document.getElementById("deletionModal");
+  numIssueToDelete = item;
+  deletionModal.hidden = false;
+}
+function cancelDeletion(){
+  var deletionModal = document.getElementById("deletionModal");
+  deletionModal.hidden = true;
+}
+function confirmDeletion(){
+  var elIssueToDelete = document.getElementById("listItem" + numIssueToDelete);
+  elIssueToDelete.parentNode.removeChild(elIssueToDelete);
+  deletionModal.hidden = true;
+}
+
+
