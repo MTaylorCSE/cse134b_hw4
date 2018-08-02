b1f87f8bfc782807a741f7f6e045bf97a1244a89
MTaylorCSE <mrt001@ucsd.edu>
2018-08-01 19:28:10 -0700

    If offline, immediately brought to offline page

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,26 @@
-self.addEventListener('fetch',function(event){
-  debugger;
+self.addEventListener('install',function(event){
+  event.waitUntil(
+    caches.open('static-v1')
+      .then(function(cache){
+        cache.add('offline.html')
+      })
+  )
 });
-console.log("nope");
+
+self.addEventListener('fetch',function(event){
+  event.respondWith(
+    fetch(event.request)
+    .catch(function(error){
+      return caches.match('offline.html')
+    }))
+  //   caches.match(event.request)
+  //     .then(function(response){
+  //       return response || fetch(event.request);
+  //     })
+  //     .catch(function(){
+  //       if(event.request.mode == 'navigate'){
+  //         return caches.match('offline.html');
+  //       }
+  //     })
+  // );
+});
